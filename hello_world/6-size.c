7c0a67c856632c4853fedfe24b888495656c2b02
Axel <axel.palombo.ap@gmail.com>
2024-06-13 14:14:58 +0000

    now its corrected the output

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 	/* Prints the size of data types */
-	printf("Size of a char: = %u\n byte(s)", sizeof(char));
-	printf("Size of an int: = %u\n byte(s)", sizeof(int));
-	printf("Size of a long int: = %u\n byte(s)", sizeof(long int));
-	printf("Size of a long long int: = %u\n byte(s)", sizeof(long long int));
-	printf("Size of a float: = %u\n byte(s)", sizeof(float));
+	printf("Size of a char: %u\n byte(s)", sizeof(char));
+	printf("Size of an int: %u\n byte(s)", sizeof(int));
+	printf("Size of a long int: %u\n byte(s)", sizeof(long int));
+	printf("Size of a long long int: %u\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %u\n byte(s)", sizeof(float));
 	return (0);
 }
