05adb1f50fe8b8e466d6507ca9fe8bd4baccaff9
Axel <axel.palombo.ap@gmail.com>
2024-06-13 13:15:13 +0000

    corrected code

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 # include <stdio.h>
 /* This script prints in screen a message*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
