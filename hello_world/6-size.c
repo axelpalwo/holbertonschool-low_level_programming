0d20af3ff50e86c0d0a6c264a19dc6d886a9027c
Axel <axel.palombo.ap@gmail.com>
2024-06-13 14:09:56 +0000

    2 bit sizes done

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 	/* Prints the size of data types */
-	printf("Size of a char: = %ld\n", sizeof(char));
-	printf("Size of an int: = %ld\n", sizeof(int));
-	printf("Size of a long int: = %ld\n", sizeof(long int));
-	printf("Size of a long long int: = %ld\n", sizeof(long long int));
-	printf("Size of a float: = %ld\n", sizeof(float));
+	printf("Size of a char: = %lu\n", sizeof(char));
+	printf("Size of an int: = %lu\n", sizeof(int));
+	printf("Size of a long int: = %lu\n", sizeof(long int));
+	printf("Size of a long long int: = %lu\n", sizeof(long long int));
+	printf("Size of a float: = %lu\n", sizeof(float));
 	return (0);
 }
