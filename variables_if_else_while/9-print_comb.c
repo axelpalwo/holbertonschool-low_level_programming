b1485245b9480fdfe9cca471e2002485e1b15bde
Axel <axel.palombo.ap@gmail.com>
2024-06-14 18:23:10 +0000

    ninth ex

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* Print from 0 to 9*/
+	int number = 48;
+
+	while (number < 58)
+	{
+		putchar(number);
+		putchar(',');
+		putchar(' ');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
