36e711e9d3da34d7d992aa2fe141ee1f142c7437
Axel <axel.palombo.ap@gmail.com>
2024-06-14 17:45:38 +0000

    sixth ex

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* Print all numbers from 0 to 10 */
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
