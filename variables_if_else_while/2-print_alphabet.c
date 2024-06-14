9d4f4d593138a6f3879d77774ad083bce5abc169
Axel <axel.palombo.ap@gmail.com>
2024-06-14 17:06:25 +0000

    second ex

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* This script starts at ASCII code number 'a' 97 and goes until 'z' at 122*/
+	char letter = "a";
+	int code = 97;
+
+	while (code <= 122)
+	{
+		putchar(letter);
+		putchar("\n");
+		letter++;
+		code++;
+	}
+}
