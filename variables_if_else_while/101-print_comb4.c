3ff9cdb76d1d8b963b07fe594fa33df9bbee068d
Axel <axel.palombo.ap@gmail.com>
2024-06-15 22:09:15 +0000

    eleventh advanced ex

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -9,60 +9,30 @@ int main(void)
 	int num1 = 48;
 	int num2 = 49;
 	int num3 = 50;
-	int counter = 0;
-	int counter2 = 0;
-	int counter3 = 0;
 
-	for ( ; counter < 9 ; )
+	while (num1 <= 55)
 	{
-		if (num1 != num2 && num3 != num2 && num1 != num3)
+		num2 = num1 + 1;
+		while (num2 <= 56)
 		{
-			for ( ; counter2 < 9 ; )
+			num3 = num2 + 1;
+			while (num3 <= 57)
 			{
-				if (num1 != num2 && num3 != num2 && num1 != num2)
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if (num1 != 55 || num2 != 56 || num3 != 57)
 				{
-					for ( ; counter3 < 9 ; counter3++)
-					{
-						if (num2 != num3)
-						{
-							putchar(num1);
-							putchar(num2);
-							putchar(num3);
-							if (num1 <= 55)
-							{
-								putchar(',');
-								putchar(' ');
-								num3++;
-							}
-							if (num3 >= 58)
-							{
-								num2++;
-								num3 = num2;
-							}
-						} else
-						{
-							if (num3 >= 58)
-							{
-								num3 = num2;
-								num2 = num1;
-							} else
-								num3++;
-						}
-					}
-					counter3 = 0;
-					counter2++;
-					if (num2 >= 57)
-					{
-						num1++;
-						num2 = num1;
-					}
+					putchar(',');
+					putchar(' ');
 				}
+				num3++;
 			}
-			counter2 = 0;
-			counter++;
-		} else
 			num2++;
+		}
+		num1++;
 	}
 	putchar('\n');
+
 	return (0);
 }
