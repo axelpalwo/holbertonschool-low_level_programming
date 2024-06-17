6199d71fa6a66cf0772ea655e688723eb26f1c9d
Axel <Axel.palombo.ap@gmail.com>
2024-06-17 19:25:23 +0000

    eighth ex

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,32 +9,33 @@ void jack_bauer(void)
 	int hours2 = 0;
 	int min1 = 0;
 	int min2 = 0;
+	int counter = 0;
 
-	while (hours1 <= 2)
+	while (counter < 1)
 	{
-		while (hours2 <= 9)
+		_putchar(hours1 + '0');
+		_putchar(hours2 + '0');
+		_putchar(':');
+		_putchar(min1 + '0');
+		_putchar(min2 + '0');
+		_putchar('\n');
+		min2++;
+		if (hours1 == 2 && hours2 == 3 && min1 == 5 && min2 == 10)
+			break;
+		if (min2 == 10)
 		{
-			while (min1 <= 5)
-			{
-				while (min2 <= 9)
-				{
-					if (hours1 == 2 && hours2 == 3 && min1 == 5 && min2 == 9)
-						break;
-					else
-					{
-						_putchar(hours1 + '0');
-						_putchar(hours2 + '0');
-						_putchar(':');
-						_putchar(min1 + '0');
-						_putchar(min2 + '0');
-						_putchar('\n');
-						min2++;
-					}
-				}
-				min1++;
-			}
-			hours2++;
+			min1++;
+			min2 = 0;
 		}
-		hours1++;
+		if (min1 == 10)
+		{
+			hours2++;
+			min1 = 0;
+		}
+		if (hours2 == 10)
+		{
+			hours1++;
+			hours2 = 0;
+		}
 	}
 }
