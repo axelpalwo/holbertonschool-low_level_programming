19e5fc9730163c2f9bd56730b22203fd61443823
Axel <Axel.palombo.ap@gmail.com>
2024-06-17 12:45:00 +0000

    first ex

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	return (0);
+}
