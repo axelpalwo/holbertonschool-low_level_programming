9a5da837f74ee295a1313f86b511cd8bc67a7f7b
Axel <Axel.palombo.ap@gmail.com>
2024-06-17 15:23:12 +0000

    corrected doc

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /*
  * _islower - Entry point
+ * @c: 'This parameter is going to be tested if its lower'
  * Description: 'This script determines if a character is lower'
- * @c: Variable for a character to test
  * Return: 1 (Success) - 0 (Otherwise)
  */
 
