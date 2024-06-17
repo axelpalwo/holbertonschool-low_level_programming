43fa6f13efbbcc8f728339da27ba37549146e38a
Axel <Axel.palombo.ap@gmail.com>
2024-06-17 18:02:12 +0000

    seventh ex

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @number: Its a whole number to be tested
+ * Return: The value of the last digit
+ */
+int print_last_digit(int number)
+{
+	number = number % 10;
+	return (number);
+}
