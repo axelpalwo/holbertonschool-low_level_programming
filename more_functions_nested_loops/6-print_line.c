db8cd2964d0a9a8549154645df79367250741a9b
root <axel.palombo.ap@gmail.com && git config --global user.name Axel>
2024-06-18 12:03:00 +0000

    sixth ex

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_line - Prints underscores in function of n
+ * @n: The times of underscores
+ * Return: None
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	} else if (n < 0)
+	{
+		while (n < 0)
+		{
+			_putchar('_');
+			n++;
+		}
+		_putchar('\n');
+	} else
+		_putchar('\n');
+}
