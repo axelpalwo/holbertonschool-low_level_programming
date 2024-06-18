3ce03d3c450f363777baa59a2ccdaf60cebf6055
root <axel.palombo.ap@gmail.com && git config user.name axel>
2024-06-18 00:20:56 +0000

    eleventh ex

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,41 +6,79 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	int var;
+
+	if (n <= 98)
 	{
-		if (n != 98)
+		while (n <= 98)
 		{
-			if (n > 9)
+			if (n != 98)
+			{
+				if (n > 9)
+				{
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				} else if (n >= 0 && n <= 9)
+				{
+					_putchar(n + '0');
+					_putchar(',');
+					_putchar(' ');
+				} else if (n < 0 && n >= -9)
+				{
+					_putchar('-');
+					_putchar((-n) + '0');
+					_putchar(',');
+					_putchar(' ');
+				} else
+				{
+					_putchar('-');
+					_putchar(((-n) / 10) + '0');
+					_putchar(((-n) % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			} else
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			} else if (n > 0 && n <= 9)
+				_putchar('\n');
+			}
+			n++;
+		}
+	} else
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
 			{
-				_putchar(n + '0');
-				_putchar(',');
-				_putchar(' ');
-			} else if (n < 0 && n >= -9)
-			{
-				_putchar('-');
-				_putchar((-n) + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (n > 99)
+				{
+					var = n % 100;
+					if (var >= 10)
+						var = var / 10;
+					_putchar((n / 100) + '0');
+					_putchar(((n % 100) / 10) + '0');
+					_putchar(var + '0');
+					_putchar(',');
+					_putchar(' ');
+					var = 0;
+				} else
+				{
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
 			} else
 			{
-				_putchar('-');
-				_putchar(((-n) / 10) + '0');
-				_putchar(((-n) % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar('\n');
 			}
-		} else
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar('\n');
+			n--;
 		}
-		n++;
 	}
+
 }
