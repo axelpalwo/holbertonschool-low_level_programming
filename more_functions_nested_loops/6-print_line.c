1004b41aa25a5ce5db0f0fe647cff5ce2d397e95
root <axel.palombo.ap@gmail.com && git config --global user.name Axel>
2024-06-18 12:08:20 +0000

    sixth ex

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,20 +6,15 @@
  */
 void print_line(int n)
 {
+	if (n < 0)
+		n = -n;
+
 	if (n > 0)
 	{
 		while (n > 0)
 		{
 			_putchar('_');
 			n--;
-		}
-		_putchar('\n');
-	} else if (n < 0)
-	{
-		while (n < 0)
-		{
-			_putchar('_');
-			n++;
 		}
 		_putchar('\n');
 	} else
