0cae33d76782c607da6d9f51f1a6d1506e542011
Axel <Axel.palombo.ap@gmail.com>
2024-06-19 17:56:27 +0000

    fourth exercise

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _puts - Prints a string
+ * @str: String
+ * Return: None
+ */
+void print_rev(char *str)
+{
+	int counter = 0;
+	int i = 0;
+
+	while (counter == 0)
+	{
+		if (str[i] != '\0')
+			i++;
+		else
+			break;
+	}
+	while (counter == 0)
+	{
+		if (i != 0)
+			_putchar(str[i]);
+		else
+		{
+			_putchar('\n');
+			break;
+		}
+		i--;
+	}
+}
