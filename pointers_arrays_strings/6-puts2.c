9c823c6c86fa506be6d884cc9149a0ac98a90678
Axel <Axel.palombo.ap@gmail.com>
2024-06-20 12:15:13 +0000

    sixth ex

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,16 @@
  */
 void puts2(char *str)
 {
-	int i;
-	int length;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	length = i;
-	for (i = 0; i < length; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
+	_putchar('\n');
 }
