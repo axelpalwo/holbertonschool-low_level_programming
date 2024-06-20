5cd011b95d302caf7c028bc39d4b22bf83c833d2
Axel <Axel.palombo.ap@gmail.com>
2024-06-20 12:40:04 +0000

    seventh ex

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "length.c"
+/**
+ * puts_half - Prints half of a string
+ * @str: String
+ * Return: None
+ */
+void puts_half(char *str)
+{
+	int len = length(str) + 1;
+
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = (len - 1) / 2;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
