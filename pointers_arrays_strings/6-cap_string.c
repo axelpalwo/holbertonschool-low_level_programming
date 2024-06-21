d2a5b074bb9bd7b46c52286d85d0fca6e0500862
axel <axel.palombo.ap@gmail.com>
2024-06-21 18:44:19 +0000

    sixth exercise

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "length.c"
+#include <stdio.h>
 /**
  * cap_string - Capitalizes all words of a string
  * @str: String
@@ -18,8 +19,10 @@ char *cap_string(char *str)
 		while (v <= lenarray)
 		{
 			if (str[i] == array[i])
+				printf("Entre en el IF");
 				if (str[i + 1] >= 97 && str[i + 1] <= 122)
 					str[i + 1] = str[i + 1] - 32;
+					printf("Cambie una letra");
 			v++;
 		}
 		v = 0;
