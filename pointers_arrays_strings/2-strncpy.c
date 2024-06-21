cbb58369a24df08c06ca796b0eaa7893ffd2bd58
Axel <Axel.palombo.ap@gmail.com>
2024-06-21 16:24:50 +0000

    second exercise

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "length.c"
+/**
+ * *_strncpy - Copies a string in destiny the number of bytes n
+ * @dest: Destiny to be reemplaced
+ * @src: Sources to be copies
+ * @n: Number of bytes copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int lensrc = length(src);
+	int i = 0;
+
+	while (i <= n)
+	{
+		if (i < lensrc)
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
