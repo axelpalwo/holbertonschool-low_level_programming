e33f195be2c47a1855574faaf8c646180a77d332
Axel <axel.palombo.ap@gmail.com>
2024-06-23 13:43:03 +0000

    seventh ex

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,14 +10,14 @@ char *leet(char *string)
 	int i = 0;
 	int v = 0;
 	int numVal[] = {0, 1, 3, 4, 7};
-	char *letterVal[] = {"O", "L", "E", "A", "T"};
+	char letterVal[] = {'O', 'L', 'E', 'A', 'T'};
 	int arrlen = sizeof(letterVal) / sizeof(letterVal[0]);
 
 	while (string[i] != '\0')
 	{
 		while (v < arrlen)
 		{
-			if (string[i] == *letterVal[v] || string[i] == (*letterVal[v] + 32))
+			if (string[i] == letterVal[v] || string[i] == (letterVal[v] + 32))
 			{
 				string[i] = numVal[v] + '0';
 				break;
