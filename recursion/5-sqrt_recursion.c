1b603579163e4f9b3ec2f0bf0839d86f5475f57e
Axel <Axel.palombo.ap@gmail.com>
2024-06-26 17:28:48 +0000

    fifth exercise

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _sqrt_recursion - Finds the natural square root of a number
+ * findroot - Finds the natural square root of a number
  * @n: Number
+ * @num: Number to be powered by itself
  * Return: Number
  */
 int findroot(int num, int n)
@@ -12,6 +13,11 @@ int findroot(int num, int n)
 		return (findroot(num + 1, n));
 	return (-1);
 }
+/**
+ * _sqrt_recursion - Finds the natural square root of a number
+ * @n: Number
+ * Return: Number
+ */
 int _sqrt_recursion(int n)
 {
 	int num1 = 0;
