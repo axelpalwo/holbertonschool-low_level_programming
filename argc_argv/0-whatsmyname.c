e48d45b3a529bde9005af52e5751d9d0a67aaf03
Axel <Axel.palombo.ap@gmail.com>
2024-06-27 12:07:49 +0000

    exercise 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 /**
  * main - Prints the name of the program
+ * @argc: Counter of arguments passed
+ * @argv: Array of arguments passed
  * Return: 0 Success
  */
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	
+
 	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
