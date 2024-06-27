e6e06834818109ecf3ae1c389383f95e47c40e63
Axel <Axel.palombo.ap@gmail.com>
2024-06-27 12:32:00 +0000

    third exercise

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Multiplies two numbers
+ * @argc: Counts arguments passed
+ * @argv: Array of arguments passed
+ * Return: Result
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i\n", result);
+	return (result);
+}
