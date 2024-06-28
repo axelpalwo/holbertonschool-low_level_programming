e35bfa0246cebd4f19c570224af6d5643a29ded6
Axel <Axel.palombo.ap@gmail.com>
2024-06-28 12:40:10 +0000

    advanced exercise

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Prints the minimum number of coins tomake change for an amount of money
+ * main - Prints the minimum number of coins tomake change for an amount money
  * @argc: Count of arguments passed
  * @argv: Array of arguments passed
  * Return: 1 (Error) - 0 (Arg negative) - Change (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	int num = atoi(argv[1]);
+	int result = 0;
 
 	if (argc > 2)
 	{
@@ -18,19 +18,35 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	if (num < 0)
-	{
 		printf("0\n");
-		return (0);
+	while (num > 0)
+	{
+		if (num / 25 > 0)
+		{
+			result += num / 25;
+			num %= 25;
+		}
+		else if (num / 10 > 0)
+		{
+			result += num / 10;
+			num %= 10;
+		}
+		else if (num / 5 > 0)
+		{
+			result += num / 5;
+			num %= 5;
+		}
+		else if (num / 2 > 0)
+		{
+			result += num / 2;
+			num %= 2;
+		}
+		else
+		{
+			result += num;
+			num -= 1;
+		}
 	}
-	if (num / 25 > 0)
-		printf("%i\n", num / 25);
-	else if (num / 10 > 0)
-		printf("%i\n", num / 10);
-	else if (num / 5 > 0)
-		printf("%i\n", num / 5);
-	else if (num / 2 > 0)
-		printf("%i\n", num / 2);
-	else
-		printf("%i\n", num);
+	printf("%i\n", result);
 	return (0);
 }
