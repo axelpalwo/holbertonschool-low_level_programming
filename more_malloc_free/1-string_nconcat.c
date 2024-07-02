e8dd5a3e7e39dd9b6145ccb3cd2ff27779bd46bc
Axel <Axel.palombo.ap@gmail.com>
2024-07-02 13:51:27 +0000

    first exercise

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,8 +11,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0;
 	unsigned int v = 0;
-	unsigned int lens1 = length(s1) - 1;
-	unsigned int lens2 = length(s2) - 1;
+	unsigned int lens1 = length(s1);
+	unsigned int lens2 = length(s2);
 	char *newstr;
 
 	if (s1 == NULL)
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			newstr[i] = s2[v];
 			i++;
 			v++;
-			n--;
+			n++;
 		}
 	} else
 	{
