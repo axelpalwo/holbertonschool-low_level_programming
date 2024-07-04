00ed8857000f1655d2e07dcc7eb980ab8b77e870
Axel <Axel.palombo.ap@gmail.com>
2024-07-04 15:43:16 +0000

    first exercise

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "dog.h"
+/**
+ * init_dog - Initializes a new dog
+ *@d: New dog instance
+ *@name: Name of the dog
+ *@age: Age of the dog
+ *@owner: Owner of the dog
+ *Return: None;
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,3 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner);
