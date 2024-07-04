c86a1b2d8ae620649f0a922780b9021964aa2356
Axel <Axel.palombo.ap@gmail.com>
2024-07-04 16:09:48 +0000

    second exercise

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_dog - Prints the attributes of dog
+ * @d - Instance of dog
+ * Return: 0 Success
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("(nil)");
+
+		if (d->age >= 0)
+			printf("Age: %f\n", d->age);
+		else
+			printf("(nil)");
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("(nil)");
+	}
+}
