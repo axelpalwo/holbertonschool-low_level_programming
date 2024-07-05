826c6cb9b0dbb770378795535f7f635ebc983c3b
Axel <axel.palombo.ap@gmail.com>
2024-07-05 13:34:41 +0000

    fourth exercise

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,25 +9,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	char dogname[] = name;
-	char owner[] = owner;
+	char *newname;
+	char *newowner;
 
+	newname = malloc(sizeof(name));
+	if (newname == NULL)
+		return (NULL);
+	strncpy(newname, name, 10);
+
+	newowner = malloc(sizeof(owner));
+	if (newowner == NULL)
+		return (NULL);
+	strncpy(newowner, owner, 10);
+	/* Malloc de la nueva estructura*/
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
+	/* Le asignamos valor al atributo name*/
+	newdog->name = newname;
 	if (newdog->name == NULL)
 	{
 		free(newdog->name);
 		free(newdog);
 	}
+	/* Le asignamos valor al atributo age */
 	newdog->age = age;
-	newdog->owner = owner;
+	/* Le asignamos valor al atributo owner */
+	newdog->owner = newowner;
 	if (newdog->owner == NULL)
 	{
 		free(newdog->owner);
 		free(newdog);
 	}
-
+	/* Retornamos la nueva estructura formada */
 	return (newdog);
 }
