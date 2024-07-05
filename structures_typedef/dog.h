8c8d0272d7c61b8823ede6a600b429a118028c1c
Axel <axel.palombo.ap@gmail.com>
2024-07-05 15:08:13 +0000

    updated dog h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,4 +21,5 @@ char *_strncpy(char *dest, char *src, int n);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
