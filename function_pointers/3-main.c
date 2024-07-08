c4233f2ae18629dc27fd8f5ec1c6a758f5e9f7b5
Axel <Axel.palombo.ap@gmail.com>
2024-07-08 17:42:55 +0000

    third exercise

---
+++ function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+/**
+ * main - Entry point
+ *@argc: Count of args
+ *@argv: Array of args
+ *Return: Result of operand
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func)(int, int);
+
+	/* Si tiene Argumentos distintos a 4 (Nombre - Num1 - Op - Num2) */
+	if (argc != 4)
+	{
+		printf("Error:\n");
+		exit(98);
+	}
+	/* Trabajamos los parametros */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	/* Si intentan dividir o modulo entre de 0*/
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* Enviamos el operando para seleccionar la funci√n de la operaco√ */
+	func = get_op_func(argv[2]);
+	/* Si el operando no esta en la lista, get_op_func retorna NULL */
+	if (*func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/* Si todo esta correcto, llamamos a la funcion y retornamos el resultado*/
+	result = func(num1, num2);
+	printf("%i\n", result);
+	return (result);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - Sum of two ints
+ * @a: Num1
+ * @b: Num2
+ * Return: Result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - difference of two ints
+ * @a: Num1
+ * @b: Num2
+ * Return: Result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Product of two ints
+ * @a: Num1
+ * @b: Num2
+ * Return: Result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of two ints
+ * @a: Num1
+ * @b: Num2
+ * Return: Result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Remainder of the division between two ints
+ * @a: Num1
+ * @b: Num2
+ * Return: Result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
