c196a3f8263add2aefba5e1a5d298dc833071a28
Axel <Axel.palombo.ap@gmail.com>
2024-07-08 14:56:31 +0000

    second exercise

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer
+ * @array: Array given
+ * @size: Size of the Array
+ * @cmp: Funct to be used to compare values
+ * Return: Index of the first element / -1 on No element Matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
