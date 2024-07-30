0f1ebe64e320ce21720b3b2ad77ed7b9930e5cbe
axel.palombo.ap@gmail.com <axel>
2024-07-30 15:13:50 +0000

    fourth exercise

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - Frees a list
+ * @head: Head of the list
+ * Return: None
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	current = head;
+	while (head != NULL)
+	{
+		free(current->str);
+		free(current->len);
+		current = current->next;
+	}
+}
