ca941f8ac7963e8c53115c599419210a59497485
Axel <Axel.palombo.ap@gmail.com>
2024-07-31 17:00:13 +0000

    second exercise

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_dnodeint - Adds a new node at the beggining of a list
+ * @head: Head of list
+ * @n: Int to be added
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
