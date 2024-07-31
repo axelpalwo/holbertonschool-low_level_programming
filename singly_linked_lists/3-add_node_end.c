7916aaab04f3dd4403af87c898825e243f5efeea
Axel <Axel.palombo.ap@gmail.com>
2024-07-31 13:01:53 +0000

    third exercise

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,21 +11,30 @@ list_t *add_node_end(list_t **head, const char *str)
 	size_t counter;
 	list_t *current;
 
-	*current = *head;
-	while (current != NULL)
-	{
-		current = current->next;
-	}
-
+	/* Asignamos memoria para el nuevo nodo */
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
+	/* Duplicamos string y asignamos len */
 	new_node->str = strdup(str);
 	for (counter = 0; *str != '\0'; str++)
 		counter++;
 	new_node->len = counter;
 	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	/* Vamos hasta el ultimo nodo */
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
+	/* Retornamos el nuevo nodo armado */
 	return (new_node);
 }
