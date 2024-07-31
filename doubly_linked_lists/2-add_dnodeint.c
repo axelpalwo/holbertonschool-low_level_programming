ef7bb5bb68c46cae57e8b38f69fd39def08ad522
Axel <Axel.palombo.ap@gmail.com>
2024-07-31 17:06:50 +0000

    second exercise

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		*head = new_node;
 		new_node->next = NULL;
+		new_node->prev = NULL;
 		return (new_node);
 	}
 	new_node->next = *head;
+	new_node->prev = NULL;
 	*head = new_node;
 
 	return (new_node);
