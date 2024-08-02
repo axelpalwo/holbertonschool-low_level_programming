a8b9b7dfc30673f9b8360fbf57a84fddf71b1adf
Axel <Axel.palombo.ap@gmail.com>
2024-08-02 15:02:21 +0000

    seventh exercise

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,11 +27,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (counter = 0; current != NULL; counter++)
 		current = current->next;
 
-	if (counter < idx)
+	if (counter + 1 < idx)
 		return (NULL);
 
 	current = *h;
-	for (counter = 0; counter < idx; counter++)
+	for (counter = 0; counter <= idx; counter++)
 		current = current->next;
 
 	current->prev->next = new_node;
