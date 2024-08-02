5f9d23b1cfb9b4c4e5b12e1ff3d63e2817d0775c
Axel <Axel.palombo.ap@gmail.com>
2024-08-02 13:57:37 +0000

    second exercise second attempt

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,4 +5,24 @@
  * @n: Int to be added
  * Return: Node
  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *current;
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	current = *head;
+	current->prev = new_node;
+	new_node->next = current;
+	*head = new_node;
+	return (new_node);
+}
