2323be2aef86f66989bec3687eaba2002e5a3d3b
Axel <Axel.palombo.ap@gmail.com>
2024-08-02 15:45:02 +0000

    seventh exercise

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,29 +16,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-
-	if (*h == NULL)
+	/* Case index = 0 (Primera posicion) */
+	if (idx == 0)
 	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
 
 	current = *h;
-	for (counter = 0; current != NULL; counter++)
+	/* Recorremos la lista hasta la pos anterior al index*/
+	for (counter = 0; current != NULL && counter < idx - 1; counter++)
 		current = current->next;
-
-	if (counter < idx)
+	/* Si el nodo es nulo, nos salimos de la lista*/
+	if (current == NULL)
 		return (NULL);
-
-	current = *h;
-	for (counter = 0; counter < idx; counter++)
-		current = current->next;
-
-	current->prev->next = new_node;
-	new_node->prev = current->prev;
-
-	current->prev = new_node;
-	new_node->next = current;
+	/* Prev & Next del nodo creado */
+	new_node->next = current->next;
+	new_node->prev = current;
+	/* Si el nodo siguiente existe, hacemos que Prev apunte a nuestro nodo */
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	/* Colocamos nuestro nodo en el next a su anterior*/
+	current->next = new_node;
 
 	return (new_node);
 }
