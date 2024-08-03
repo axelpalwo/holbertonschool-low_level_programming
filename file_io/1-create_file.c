4be5502082100a77a87f67c294987be1a65dd117
axel <axel.palombo.ap@gmail.com>
2024-08-03 14:41:46 +0000

    first exercise

---
+++ file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file
+ * @text_content: Text content in the file
+ * Return: 1 Success / -1 Failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, strlen(text_content));
+	close(fd);
+	return (1);
+}
